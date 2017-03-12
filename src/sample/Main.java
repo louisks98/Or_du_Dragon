66055d69cc83614a18748696763801cd63021653
louisks98 <louis.christophe.poulin@gmail.com>
2017-03-12 16:04:06 -0400

    affichage BackGround et liaison

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,16 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Vector;
+import javafx.scene.image.Image;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
-    public void Cree_Noeud(Group g, ArrayList<String> coord, Vector<Boolean> construsible)
+    public void Cree_Noeud(Pane g, ArrayList<String> coord, Vector<Boolean> construsible)
     {
         int x;
         int y;
@@ -30,6 +36,49 @@ public class Main extends Application {
                 c.setStrokeWidth(1);
             }
             g.getChildren().add(c);
+        }
+    }
+
+    public void CreeLiens(Pane g, ArrayList<String> coord, Vector<String> Liaison)
+    {
+        String line;
+        String node;
+        String otherNodes;
+        int x1;
+        int x2;
+        int y1;
+        int y2;
+
+        for(int i = 0; i < Liaison.size(); i++)
+        {
+            int pos;
+            boolean terminer = false;
+            line = Liaison.get(i);
+            node = line.substring(0, line.indexOf(" ")).trim();
+            x1 = getCoordonneeX(coord.get(Integer.parseInt(node)));
+            y1 = getCoordonneeY(coord.get(Integer.parseInt(node)));
+            otherNodes = line.substring(line.indexOf(" ") + 1, line.length());
+            do
+            {
+                pos = otherNodes.indexOf(" ");
+                if (pos != -1)
+                {
+                    node = otherNodes.substring(0, pos).trim();
+                    x2 = getCoordonneeX(coord.get(Integer.parseInt(node)));
+                    y2 = getCoordonneeY(coord.get(Integer.parseInt(node)));
+                    otherNodes = otherNodes.substring(otherNodes.indexOf(" ") + 1, otherNodes.length());
+                }
+                else
+                {
+                    node = otherNodes;
+                    x2 = getCoordonneeX(coord.get(Integer.parseInt(node)));
+                    y2 = getCoordonneeY(coord.get(Integer.parseInt(node)));
+                    terminer = true;
+                }
+                Line l = new Line(x1, y1, x2, y2);
+                g.getChildren().add(l);
+
+            }while(!terminer);
         }
     }
 
@@ -63,16 +112,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         LireServeur serveur = new LireServeur();
-        Group root = new Group();
+        Pane root = new Pane();
         serveur.LireNoeuds_Arcs();
         Cree_Noeud(root ,serveur.GetCoordonnee(), serveur.GetConstruisible());
+        CreeLiens(root, serveur.GetCoordonnee(), serveur.GetLiaison());
 
+        Image image = new Image("file:nowhereland.png");
+        BackgroundImage bg = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        root.setBackground(new Background(bg));
         primaryStage.setTitle("L'Or du Dragon");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
